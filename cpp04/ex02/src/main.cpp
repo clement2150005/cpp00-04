74f104ec0f362a2ba2628d5615dbf91484af70e8
ccolin <clmnt.co@gmail.com>
2025-06-02 12:47:16 +0900

    Quick commit

--- cpp04/ex02/src/main.cpp
+++ cpp04/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ccolin <ccolin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 15:58:58 by ccolin            #+#    #+#             */
-/*   Updated: 2025/04/09 10:38:11 by ccolin           ###   ########.fr       */
+/*   Updated: 2025/06/02 12:46:42 by ccolin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,3 +21,30 @@ int main()
 	Animal* animal = new Animal(); // Instantiation of Animal class should not work
 	return 0;
 }
+
+// int main()
+// {
+// 	const Animal* catsAndDogs[100];
+	
+// 	for (int i = 0; i < 50; i++)
+// 		catsAndDogs[i] = new Cat();
+// 	for (int i = 50; i < 100; i++)
+// 		catsAndDogs[i] = new Dog();
+// 	for (int i = 0; i < 100; i++)
+// 		delete catsAndDogs[i];
+
+// 	Animal* strayCat = new Cat();
+// 	strayCat->setIdea("Chase mouse", 0);
+		
+// 	Animal* houseCat = new Cat(*(dynamic_cast<Cat*>(strayCat)));
+		
+// 	houseCat->setIdea("Eat treats", 0);
+		
+// 	strayCat->think(0);    // should print "Chase mouse"
+// 	houseCat->think(0);    // should print "Eat treats"
+		
+// 	delete strayCat;
+// 	delete houseCat;
+		
+// 	return 0;
+// }
